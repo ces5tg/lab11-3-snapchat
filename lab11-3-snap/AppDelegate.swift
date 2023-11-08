da29834241d924c9cb0510f22c992732b3b10b03
cesar <cesar.pacho@tecsup.edu.pe>
2023-11-07 20:08:13 -0500

    final - errores en google

--- lab11-3-snap/AppDelegate.swift
+++ lab11-3-snap/AppDelegate.swift
@@ -7,11 +7,17 @@
 
 import UIKit
 import FirebaseCore
+import FirebaseAuth
+import GoogleSignIn
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    func application(_ app: UIApplication,
+                     open url: URL,
+                     options: [UIApplication.OpenURLOptionsKey: Any] = [:]) -> Bool {
+      return GIDSignIn.sharedInstance.handle(url)
+    }
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
--- lab11-3-snap/iniciarSesionViewController.swift
+++ lab11-3-snap/iniciarSesionViewController.swift
@@ -7,6 +7,8 @@
 
 import UIKit
 import FirebaseAuth
+import FirebaseCore
+import GoogleSignIn
 class iniciarSesionViewController: UIViewController {
    
 
@@ -14,6 +16,8 @@ class iniciarSesionViewController: UIViewController {
     @IBOutlet weak var emailTextField: UITextField!
     
     @IBAction func IniciarSesionTapped(_ sender: Any) {
+        
+        
         Auth.auth().signIn(withEmail: emailTextField.text! , password:passwordTextField.text!){(user , error) in
                     print("intentando iniciar sesion")
                     if error != nil{
@@ -26,8 +30,41 @@ class iniciarSesionViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        guard let clientID = FirebaseApp.app()?.options.clientID else { return }
+
+        // Create Google Sign In configuration object.
+        let config = GIDConfiguration(clientID: clientID)
+        GIDSignIn.sharedInstance.configuration = config
+
+        // Start the sign in flow!
+        GIDSignIn.sharedInstance.signIn(withPresenting: self) { [unowned self] result, error in
+          guard error == nil else {
+            // ...
+              return
+          }
+
+          guard let user = result?.user,
+            let idToken = user.idToken?.tokenString
+          else {
+            // ...
+              return
+          }
+
+          let credential = GoogleAuthProvider.credential(withIDToken: idToken,
+                                                         accessToken: user.accessToken.tokenString)
+
+            Auth.auth().signIn(with: credential) { result, error in
+
+              // At this point, our user is signed in
+            }
+                
+        }
     }
+    
+    
 
 
+
+    
 }
 
